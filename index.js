22d4e704b3a8cc05ef1f0a66afd0f1a6fbef31e6
Taha <taha.othman@outlook.com>
2023-03-06 11:04:41 -0500

    all functions pass the test

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function appendCat(name){
+    const newcats =[...cats, name]; 
+    return newcats;
+}
+
+function prependCat(name){
+    const newprependcats =[name, ...cats,]
+    return newprependcats;
+}
+
+function removeLastCat() {
+    return cats.slice(0,-1);
+}
+
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
